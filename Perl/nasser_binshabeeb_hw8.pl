0f3dd780ad6df3822d386b76bfbdecc163099f0e
NasserDos <nasserbinshabeeb@mail.weber.edu>
2017-04-24 08:53:11 -0600

    Almost finished Perl

--- Perl/nasser_binshabeeb_hw8.pl
+++ Perl/nasser_binshabeeb_hw8.pl
@@ -2,5 +2,42 @@
 
 use strict;
 use warnings;
+use File::Path;
+use LWP::Simple;
 
-print "Hi \n";
+
+sub copyFile{
+(my $sec,my $min,my $hour,my $mday,my $mon,my $year,my $wday,my $yday,my $isdst) = localtime();
+
+my $cmd='scp';
+my $from='nb19445@icarus.cs.weber.edu:/home/hvalle/submit/cs3030/files/FRED.csv';
+my $to = sprintf("fredData/%02d",$mon);
+
+system($cmd,$from,$to);
+
+}
+
+
+sub prepStruct{
+
+    print "Preparing file structure";
+    foreach my $i ("01".."12") {
+        mkpath "fredData/$i" 
+    }
+}
+
+
+
+sub main{
+
+    prepStruct();
+    copyFile();
+}
+
+
+
+
+
+main()
+
+
