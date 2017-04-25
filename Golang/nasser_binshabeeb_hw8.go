82eff4134fd21f91b656625a94a6b49b02d02bb0
NasserDos <nasserbinshabeeb@mail.weber.edu>
2017-04-25 13:08:57 -0600

    completed Go file struct

--- Golang/nasser_binshabeeb_hw8.go
+++ Golang/nasser_binshabeeb_hw8.go
@@ -4,10 +4,31 @@ package main
 
 import (
 		"fmt"
-
+        "os"
 	)
 
-	func main(){
-		fmt.Printf("Hi")
+    func copyFile(){
 
+    }
+
+    func prepStructure(){
+
+        fmt.Printf("Checking folder structure")
+		fmt.Printf("Creating file structure...\n")
+        os.Mkdir("fredData",os.ModePerm)
+        for i:= 1; i< 13; i++ {
+            dig := fmt.Sprintf("%02d",i)
+            os.Mkdir("fredData/"+dig,os.ModePerm)
+        }
+
+    }
+
+    func usage(){
+        fmt.Printf("usage : go run nasser_binshabeeb_hw8.go \n")
+        fmt.Printf("options -c fredData -f FRED.csv \n")
+        fmt.Printf("Both fields are required \n")
+    }
+
+
+	func main(){
 	}
