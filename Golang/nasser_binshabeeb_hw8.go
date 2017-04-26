8dbcd76d5dfd592441ecfffd3dfac3b3aa98e9c0
NasserDos <nasserbinshabeeb@mail.weber.edu>
2017-04-25 20:26:58 -0600

    Golang script complete

--- Golang/nasser_binshabeeb_hw8.go
+++ Golang/nasser_binshabeeb_hw8.go
@@ -3,23 +3,47 @@
 package main
 
 import (
-		"fmt"
-        "os"
+		"fmt" //to print in proper format
+        "os" //to create directories
+        "os/exec" //to scp
+        "time" //for time
+        "flag" //getopts
 	)
 
-    func copyFile(){
+    func copyFile(c string, f string){
+
+	    ourTime := time.Now().String()
+        var stamp  = ourTime[0:10]
+        var month = c+"/"+stamp[5:7]
+        var dest = month+"/FRED.csv."+stamp
+        var server = "nb19445@icarus.cs.weber.edu"
+        var fileAt = ":/home/hvalle/submit/cs3030/files/"+f
+        var target = server+fileAt
+
+        output, err := exec.Command("scp",target,dest).CombinedOutput()
+        if err != nil {
+              fmt.Println("Failed to copy file")
+              fmt.Println("File doesn't exist !")
+              os.Exit(1)
+          }
+          fmt.Printf(string(output))
+          fmt.Println("File transfer complete")
+          fmt.Println("File Located in [",dest,"]")
 
     }
 
     func prepStructure(){
 
-        fmt.Printf("Checking folder structure")
+        fmt.Printf("Checking folder structure\n")
 		fmt.Printf("Creating file structure...\n")
+
         os.Mkdir("fredData",os.ModePerm)
+
         for i:= 1; i< 13; i++ {
-            dig := fmt.Sprintf("%02d",i)
-            os.Mkdir("fredData/"+dig,os.ModePerm)
+                dig := fmt.Sprintf("%02d",i)
+                os.Mkdir("fredData/"+dig,os.ModePerm)
         }
+		fmt.Printf("File stucture build complete...\n")
 
     }
 
@@ -31,4 +55,20 @@ import (
 
 
 	func main(){
-	}
+        //basically getopts
+        Cust := flag.String("c","","Customer folder")
+        Data := flag.String("f","","Data File")
+        Help := flag.Bool("help",false,"Help")
+        flag.Parse()
+
+
+        if *Cust == "" || *Data == "" || *Help {
+
+            usage()
+            os.Exit(1)
+        } else{
+            prepStructure()
+            copyFile(*Cust,*Data)
+        }
+      }
+
